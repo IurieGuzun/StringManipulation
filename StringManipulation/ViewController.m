13deb02be14de6a7b0bd2bee50998e670942b605
Iurie Guzun <iurie_guzun@yahoo.com>
2020-06-05 13:29:03 -0400

    Locate Error?

--- StringManipulation/ViewController.m
+++ StringManipulation/ViewController.m
@@ -12,12 +12,34 @@
 
 @end
 
+NSMutableString *currentValue;
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+     currentValue = [NSMutableString stringWithString:@""];
+    [currentValue appendString: @"3"];
+    NSLog(@"%@", currentValue);
+    [currentValue appendString: @"2"];
+    NSLog(@"%@", currentValue);
+    
+    if ([currentValue length]>0)
+    {
+        NSString *string1 = @"This is a string";
+        NSMutableString *string2;
+        
+        string2 = [NSMutableString stringWithString: string1];
+        
+        NSString *tempValue;
+        tempValue = [currentValue substringToIndex:[currentValue length]-1];
+        currentValue = [NSMutableString stringWithString: tempValue];
+        // Straniu : Aceste doua linii par identice dar una depisteaza Error alta Nu! ===!!!!!!!!!!!!!!
+   //  currentValue = [NSMutableString stringWithString: tempValue];
+        
+        NSLog(currentValue);
+    }
+     [currentValue appendString: @"2"];
+    NSLog(currentValue);
 }
-
-
 @end
